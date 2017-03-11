fdc838ad1be714d22c73e2ddafd31e0ecbd569d0
杨宇 <yang_19840821@163.com>
2017-03-11 10:45:29 +0800

    第四次提交

--- src/com/turing/Test000.java
+++ src/com/turing/Test000.java
@@ -2,6 +2,8 @@ package com.turing;
 
 public class Test000 {
 	public static void main(String[] args) {
-		System.out.println("111111111111111111");
+		System.out.println("11111");
+		System.out.println("22222");
+		System.out.println("33333");
 	}
 }
